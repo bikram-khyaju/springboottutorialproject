96d0ad3d80616b52bd8ea246e0c78c06c5b27ae8
bikram.khyaju <khyajubikram@gmail.com>
2022-05-17 10:55:36 -0500

    first commit for springboot tutorial learing process

---
+++ src/main/java/com/dailycodebuffer/springboottutorial/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springboottutorial.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String helloworld(){
+        return "Welcome to Daily code buffer";
+    }
+}

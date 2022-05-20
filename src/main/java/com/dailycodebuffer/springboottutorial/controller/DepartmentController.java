89bf3b02bed6088bc55c0f4af2cd7a9c51ad3d76
bikram.khyaju <khyajubikram@gmail.com>
2022-05-20 09:58:29 -0500

    Lambok implemented in Department entity to remove getter, seetter and constructors

--- src/main/java/com/dailycodebuffer/springboottutorial/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/springboottutorial/controller/DepartmentController.java
@@ -1,12 +1,10 @@
 package com.dailycodebuffer.springboottutorial.controller;
-
 import com.dailycodebuffer.springboottutorial.entity.Department;
 import com.dailycodebuffer.springboottutorial.service.DepartmentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
